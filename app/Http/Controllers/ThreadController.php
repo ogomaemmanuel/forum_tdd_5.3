c7dea9a21fc932ebe42b475ac9fc2e027a8a2058
Emmanuel <ogoma.emmanuel@gmail.com>
2018-07-20 21:22:15 +0300

    first commit

---
+++ app/Http/Controllers/ThreadController.php
@@ -0,0 +1,76 @@
+<?php
+namespace App\Http\Controllers;
+use App\Thread;
+use Illuminate\Http\Request;
+class ThreadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\thread  $thread
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Thread $thread)
+    {
+        //
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\thread  $thread
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Thread $thread)
+    {
+        //
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\thread  $thread
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, thread $thread)
+    {
+        //
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\thread  $thread
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Thread $thread)
+    {
+        //
+    }
+}
